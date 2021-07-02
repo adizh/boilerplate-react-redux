2878214926b0897a2b96b0f7139941eb487da046
adizh <83157462+adizh@users.noreply.github.com>
2021-07-02 16:57:05 +0600

    Delete index.js

--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore,compose,applyMiddleware} from 'redux';
-import rootReducer from "./reducers";
-import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from 'redux-thunk'
-const composeFunc=process.env.NODE_ENV==='development'?composeWithDevTools:compose
-const composeEnhancer=composeFunc(applyMiddleware(thunk))
-const store=createStore(rootReducer(),composeEnhancer)
-export default store
